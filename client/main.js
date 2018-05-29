421c5eb2e9a2f14e058e420921b888716688aaf8
Alvaro <alvaroph@gmail.com>
2018-05-29 14:25:06 +0200

    ultimos cambios

--- client/main.js
+++ client/main.js
@@ -44,6 +44,18 @@ Template.equips.events({
   'click  .bt4'(event, instance) {
     Equips.update({"_id":4},{$inc:{nvots:1}});
   },
+   
+  'click  .bt5'(event, instance) {
+    Equips.update({"_id":5},{$inc:{nvots:1}});
+  },
+   
+  'click  .bt6'(event, instance) {
+    Equips.update({"_id":6},{$inc:{nvots:1}});
+  },
+   
+  'click  .bt7'(event, instance) {
+    Equips.update({"_id":7},{$inc:{nvots:1}});
+  },
 });
 
 Template.vots.helpers({
@@ -58,12 +70,24 @@ Template.vots.helpers({
 posicio(n){
   return n+1;
 },
-color(n){
-  const colors=['bg-success','bg-warning','bg-success','bg-success'];
+colorxx(n){
+  const colors=['bg-primary','bg-secondary','bg-success','bg-danger','bd-warning','bg-info','bg-dark'];
   return colors[n];
 },
  equips() {
 
+  const datos=[{nom: 'Autify', colorBot :"btn-primary" , classBot : "bt1"},
+  {nom: 'ManifestApp', colorBot :"btn-secondary", classBot : "bt2"},
+  {nom: 'BarcelonApp', colorBot :"btn-success", classBot : "bt3" },
+  {nom: 'TAccess', colorBot :"btn-danger" , classBot : "bt4"},
+  {nom: 'eTorn', colorBot :"btn-warning", classBot : "bt5" },
+  {nom: 'BerryLan', colorBot :"btn-info", classBot : "bt6" },
+  {nom: 'Docker4All', colorBot :"btn-dark" , classBot : "bt7"}
+  ];
+
+  const colores=['bg-primary','bg-secondary','bg-success','bg-danger','bd-warning','bg-info','bg-dark'];
+
+  //CODIGO PARA CALCULAR EL MÁXIMO DE VOTOS
   const eq2=Equips.find({},{nvots:1,_id:0},{sort: {nvots:-1}, limit:1});
     let max=0;
     eq2.forEach((equip) => {
@@ -74,8 +98,13 @@ color(n){
   const eq=Equips.find({}, {sort: {nvots: -1}});
   
   let arEquipos=[];
-  eq.forEach((equip) => {
-    var a={ "equip": equip.equip, "nvots":equip.nvots, "pct":parseInt((equip.nvots/max)*100)};
+  eq.forEach((equip,index) => {
+    var a={ "equip": equip.equip, 
+    "nvots":equip.nvots, 
+    "pct":parseInt((equip.nvots/max)*100),
+    "color" : colores[equip.equip-1],
+    "nom" : datos[equip.equip-1].nom,
+    "imatge": "pic-"+equip.equip+".png"};
     console.log(a)
     arEquipos.push(a)
   });
